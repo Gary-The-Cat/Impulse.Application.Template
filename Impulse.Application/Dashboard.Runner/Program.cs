5e4d5efb8e3fd2fd6d83a228c3d8218cf1a6d30b
Luke Berry <luke.berry@maptek.com.au>
2022-07-22 10:16:55 +0930

    Initial Commit for runner and application configuration (Not running)

---
+++ Impulse.Application/Dashboard.Runner/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Diagnostics;
+
+const string DashboardLocation = @"[Path]\[To]\Impulse.Dashboard.exe";
+var applicationDirectory = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+
+Process.Start(DashboardLocation, $"--application {applicationDirectory}");
---
+++ Impulse.Application/Impulse.Application/Application.cs
@@ -0,0 +1,42 @@
+﻿
+using Impulse.Shared.Application;
+using Ninject;
+
+namespace Impulse.Application
+{
+    public class Application : IApplication
+    {
+        private WeakReference<IKernel> kernelReference;
+
+        public Application(IKernel kernel)
+        {
+            this.kernelReference = new WeakReference<IKernel>(kernel);
+        }
+
+        // The kernel can safely be assumed alive for the duration of the application.
+        public IKernel GetKernel()
+        {
+            _ = kernelReference.TryGetTarget(out var kernel);
+            return kernel;
+        }
+
+        public string DisplayName => Properties.Resources.Title_MyApplication;
+
+        public Uri Icon => new Uri("pack://application:,,,/Impulse.Application;Component/Resources/DefaultIcon.png");
+
+        public Task Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LaunchApplication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnClose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
