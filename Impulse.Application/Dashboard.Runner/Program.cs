037b71139587b1c6fd597fa9f8e0a084ccdf7d8c
GaryTheCat <lukeberry9919@gmail.com>
2022-07-30 17:34:41 +0930

    Cleaned up & simplified the installer project for new applications, updated the license.

--- Impulse.Application/Dashboard.Runner/Program.cs
+++ Impulse.Application/Dashboard.Runner/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Diagnostics;
+﻿const string DashboardLocation = @"C:\Program Files\Impulse Dashboard\Impulse.Dashboard.exe";
 
-const string DashboardLocation = @"C:\Program Files\Impulse Dashboard\Impulse.Dashboard.exe";
-var applicationDirectory = @"C:\Users\lukeb\Documents\Impulse.Application.Template\Impulse.Application\Impulse.Application\bin\Debug\net6.0-windows";
+var applicationDirectory =
+    @"[Path][To][Repo]\Impulse.Application.Template\Impulse.Application\Impulse.Application\bin\Debug\net6.0-windows";
 
-Process.Start(DashboardLocation, $"--application {applicationDirectory}");
+System.Diagnostics.Process.Start(DashboardLocation, $"--application {applicationDirectory}");
